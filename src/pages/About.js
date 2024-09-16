07417b8251fe0a4441a84ef0e68cc638e9acbb44
Babycolonel <orlandolimbusiness@gmail.com>
2024-09-16 08:30:58 +0700

    d

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import "../About.css";
+import "../Home.css";
+
+import React, { useEffect, useState, createContext, useContext } from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+const About = () => {
+
+  };
+export default About;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+
+ import ReactDOM from "react-dom/client"
+ import React, { useEffect, useState, createContext, useContext } from 'react';
+ import "../Home.css";
+ import "../About.css";
+ import "./Layout";
+
+
+const Home = () => {
+  return (
+    <>
+
+    </>
+  );
+}
+export default Home;
