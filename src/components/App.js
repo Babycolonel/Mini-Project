7208a60e43abefdee8a6fb1cab9a0016ff7488f2
macshire <maxmacshire@gmail.com>
2024-09-16 11:05:40 +0700

    app.js

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import Home from "./Home";
+import About from "./About";
+import Layout from "./Layout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
