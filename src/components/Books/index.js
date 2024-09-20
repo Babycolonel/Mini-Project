fef75ba169d84ec777a0336d70303565a1ffcd9c
Babycolonel <orlandolimbusiness@gmail.com>
2024-09-20 09:05:44 +0700

    Click to about work

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,7 +1,11 @@
 import { Button } from '@mui/material';
 import '../Home.css';
-
+import { Outlet, Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 const Story = ({ story, onArchive }) => {
+
+  const navigate = useNavigate();
+
     const {
       title,
       url,
@@ -12,13 +16,14 @@ const Story = ({ story, onArchive }) => {
       objectID,
     } = story;
 
-    const handleClick = () => {
-      console.log('work');
+    const handleClick = (id) => {
+      console.log('work', id);
+      navigate('/about')
     }
   
     /* layout of the UI when displaying the state from the store */
     return (
-      <div className='homePageContainer' onClick={handleClick}>
+      <div className='homePageContainer' onClick={() => handleClick(objectID)}>
         <div className="story">
             <img src={image} className='bookImage'></img>
             <br></br>
