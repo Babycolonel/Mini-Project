904daf36e0f3437a0fdbeada0b24e4da705ac023
macshire <maxmacshire@gmail.com>
2024-09-23 10:08:46 +0700

    trying layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,19 +17,29 @@ import store from "../store";
 // });
 
 const Layout = ({ stories }) => {
-  const [age, setAge] = React.useState('');
-  // const [input, setInput] = React.useState('');
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  
-    const titleInput  = document.getElementById("searchBar");
-    const title = titleInput.value;
-    store.dispatch({type: SEARCH_BOOK, title })
-    console.log("title is " + title);
-    if ("title != to any titles in list") {
-      //change placeholder to something like "0 search results"
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  // Load search term from local storage on mount
+  React.useEffect(() => {
+    const storedSearchTerm = localStorage.getItem('searchTerm');
+    if (storedSearchTerm) {
+      setSearchTerm(storedSearchTerm);
     }
+  }, []);
+
+  const handleChange = (event) => {
+    const title = event.target.value;
+    setSearchTerm(title);
+    
+    // Store the search term in local storage
+    localStorage.setItem('searchTerm', title);
+    
+    // Dispatch the search action
+    store.dispatch({ type: SEARCH_BOOK, title });
   };
+
+  const filteredStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   // React.useEffect (() => {
   //   store.dispatch({ type: SEARCH_BOOK, title: input });
@@ -43,11 +53,13 @@ const Layout = ({ stories }) => {
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/bookmark">BookMark</Link>
-                <form onSubmit={handleChange}>
-                  <input type="text" id='searchBar' placeholder="Find a book for yourself!" />
-                </form>
-
-                  {/* <InputCustom onChange={(e) => setInput(e.target.value)} value={input} /> */}
+                <input
+                  type="text"
+                  id='searchBar'
+                  placeholder="Find a book for yourself!"
+                  value={searchTerm}
+                  onChange={handleChange}
+                />
               </div>
             </li>
             
