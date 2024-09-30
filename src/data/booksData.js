211134b387211be70c35da88396a715e8d463c8e
macshire <maxmacshire@gmail.com>
2024-09-30 08:49:44 +0700

    commit

--- src/data/booksData.js
+++ src/data/booksData.js
@@ -7,7 +7,8 @@ const booksData = [
         "num_comments": 3,
         "points": 4,
         "objectID": 0,
-        "genre": "Adventure"
+        "genre": "Adventure",
+        "rank": 2
     },
     {
         "image": "https://m.media-amazon.com/images/I/81R2N4PRuUL._AC_UF1000,1000_QL80_.jpg",
@@ -17,7 +18,8 @@ const booksData = [
         "num_comments": 2,
         "points": 5,
         "objectID": 1,
-        "genre": "Children"
+        "genre": "Children",
+        "rank": 1
     },
     {
         "image": "https://preview.redd.it/fgy14jrd3af41.jpg?auto=webp&s=4425fb0240637ce6bbeec7fdc49ce5d4718323f8",
@@ -25,9 +27,10 @@ const booksData = [
         "url": "https://dunenovels.com/",
         "author": "Frank Herbet",
         "num_comments": 2,
-        "points": 6,
+        "points": 3,
         "objectID": 2,
-        "genre": "Fantasy"
+        "genre": "Fantasy",
+        "rank": 3
     },
     {
         "image": "https://i.ebayimg.com/images/g/QTUAAOSwIOtbYt82/s-l1200.jpg",
@@ -35,50 +38,34 @@ const booksData = [
         "url": "https://stepheniemeyer.com/the-twilight-saga/",
         "author": "Stephenie Meyer",
         "num_comments": 9,
-        "points": 8,
+        "points": 1,
         "objectID": 3,
-        "genre": "Romance"
+        "genre": "Romance",
+        "rank": 4
     },
     {
         "image": "https://m.media-amazon.com/images/I/91Gy28f5M0L._AC_UF1000,1000_QL80_.jpg",
-        "title": "Boy In The Stripped",
+        "title": "Boy In The Striped Pjs",
         "url": "https://stepheniemeyer.com/the-twilight-saga/",
         "author": "John Boyne",
         "num_comments": 9,
-        "points": 8,
+        "points": 5,
         "objectID": 4,
-        "genre": "Literature"
+        "genre": "Literature",
+        "rank": 5
     },
     {
-        "image": "https://m.media-amazon.com/images/I/71gl2fmAp7L._AC_UF1000,1000_QL80_.jpg",
+        "image": "https://upload.wikimedia.org/wikipedia/en/d/db/The_Maze_Runner_cover.png",
         "title": "The Maze Runner",
         "url": "https://www.penguinrandomhouse.com/series/MZR/the-maze-runner-series/",
         "author": "James Dashner",
         "num_comments": 15,
-        "points": 8,
+        "points": 3,
         "objectID": 5,
-        "genre": "Adventure"
-    },
-    {
-        "image": "https://m.media-amazon.com/images/I/71hbDMgo6rL._AC_UF1000,1000_QL80_.jpg",
-        "title": "The Scorch Trials",
-        "url": "https://www.penguinrandomhouse.com/series/MZR/the-maze-runner-series/",
-        "author": "James Dashner",
-        "num_comments": 6,
-        "points": 5,
-        "objectID": 6,
-        "genre": "Adventure"
-    },
-    {
-        "image": "https://m.media-amazon.com/images/I/91vaqmbaF4L._AC_UF1000,1000_QL80_.jpg",
-        "title": "The Death Cure",
-        "url": "https://www.penguinrandomhouse.com/series/MZR/the-maze-runner-series/",
-        "author": "James Dashner",
-        "num_comments": 11,
-        "points": 9,
-        "objectID": 7,
-        "genre": "Adventure"
+        "genre": "Adventure",
+        "rank": 6
     }
 ]
+
 
 export default booksData;
