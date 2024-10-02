dde27dae240d73a4e93ff749cc1ece9d2c14d3d0
macshire <maxmacshire@gmail.com>
2024-10-02 13:42:02 +0700

    converting home page from local to DB

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,75 +15,152 @@ import { FILTER_GENRE } from "../constants/actionTypes";
 import { Action } from "@remix-run/router";
 import { SEARCH_BOOK } from "../constants/actionTypes";
 import booksData from "../data/booksData";
+import axios from "axios";
+import { useBootstrapBreakpoints } from "react-bootstrap/esm/ThemeProvider";
 
 
 const Home = ({ stories }) => {
+  // //local storage
+  // useEffect(() => {
+  //   //store the booksData in localStorage
+  //   if (!localStorage.getItem('books')) {
+  //     localStorage.setItem('books', JSON.stringify(booksData));
+  // }
+
+  // //retrieve books from localStorage and set state
+  // const localBooks = JSON.parse(localStorage.getItem('books'));
+  // setBooks(localBooks || []);
+  // //empty array dependency so it only runs once
+  // }, []);
+
+  //local storage
+  // useEffect(() => {
+    // let filteredBooks = JSON.parse(localStorage.getItem('books'));
+
+    // //filter by genre if a specific genre is selected
+    // if (age !== '' && age !== 'All') {
+    //   filteredBooks = filteredBooks.filter(book => book.genre === age);
+    // }
+
+    //filter by search term
+    //checks for both book title and author input
+  //   if (searchTerm) {
+  //     setShow(false);
+  //     filteredBooks = filteredBooks.filter(book => 
+  //       book.title.toLowerCase().includes(searchTerm.toLowerCase())
+  //       || book.author.toLowerCase().includes(searchTerm.toLowerCase())
+  //     );
+  //   }
+
+  //   //set filtered list
+  //   setBooks(filteredBooks);
+  //   if (filteredBooks.length == 0) {
+  //     console.log("filtered books nothing " );
+  //     setShow(true);
+  //   }
+  //   else {
+  //     setShow(false);
+  //   }
+  // }, [age, searchTerm]);
+
+  // //local storage
+  // const handleChange = (event) => {
+  //   const genre = event.target.value;
+  //   console.log("EVENT TARGET VALUE = " + event.target.value);
+  //   setAge(genre);
+
+  //   //filter by genre and update state
+  //   //if filter "All" display all books in local storage
+  //   if (genre === 'All'){
+  //     console.log("IT IS ALL BOOKS");
+  //     const allBooks = JSON.parse(localStorage.getItem('books'));
+  //     setBooks(allBooks);
+  //   }
+  //   //else display normal filtered books based on genre
+  //   else {
+  //      const filteredBooks = JSON.parse(localStorage.getItem('books')).filter(book => book.genre === genre);
+  //      setBooks(filteredBooks);
+  //   }
+  // };
+
   const [books, setBooks] = useState([]);
   const [age, setAge] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
   const [show, setShow] = useState(false);
 
+  //DB, getting books from DB
   useEffect(() => {
-    //store the booksData in localStorage
-    if (!localStorage.getItem('books')) {
-      localStorage.setItem('books', JSON.stringify(booksData));
-  }
+    axios.get('http://localhost:7000/books')
+      .then(response => {
+        setBooks(response.data); // Update state with the user data
+        console.log("setBooks = " + response.data);
+      })
+      .catch(error => {
+        //console.error('There was an error fetching the users!', error);
+      });
+}, [])
 
-  //retrieve books from localStorage and set state
-  const localBooks = JSON.parse(localStorage.getItem('books'));
-  setBooks(localBooks || []);
-  //empty array dependency so it only runs once
-  }, []);
-
-  //lock in time
-
+  //DB, filtering books based on DB
   useEffect(() => {
-    let filteredBooks = JSON.parse(localStorage.getItem('books'));
+    axios.get('http://localhost:7000/books')
+      .then(response => {
+        let filteredBooks = response.data;
 
-    //filter by genre if a specific genre is selected
-    if (age !== '' && age !== 'All') {
-      filteredBooks = filteredBooks.filter(book => book.genre === age);
-    }
+        //filter by genre when one of the options is clicked that is not '' and 'All'
+        if (age !== '' && age !== 'All') {
+          filteredBooks = filteredBooks.filter(book => book.genre === age);
+        }
 
-    //filter by search term
-    //checks for both book title and author input
-    if (searchTerm) {
-      setShow(false);
-      filteredBooks = filteredBooks.filter(book => 
-        book.title.toLowerCase().includes(searchTerm.toLowerCase())
-        || book.author.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    }
+        //filter by both title and author through search bar 
+        if (searchTerm) {
+          //reset show state
+          setShow(false); 
+          //set filteredBooks to either title or author
+          filteredBooks = filteredBooks.filter(book => 
+            book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            book.author.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+        }
 
-    //set filtered list
-    setBooks(filteredBooks);
-    if (filteredBooks.length == 0) {
-      console.log("filtered books nothing " );
-      setShow(true);
-    }
-    else {
-      setShow(false);
-    }
+        //set the filtered list of books
+        setBooks(filteredBooks);
+
+        //show message if no book/author found
+        if (filteredBooks.length === 0) {
+          setShow(true);
+        } else {
+          setShow(false);
+        }
+      })
+      .catch(error => {
+        console.error('There was an error filtering the books!', error);
+      });
   }, [age, searchTerm]);
 
+  //DB, handling change in genre
   const handleChange = (event) => {
     const genre = event.target.value;
     console.log("EVENT TARGET VALUE = " + event.target.value);
     setAge(genre);
 
-    //filter by genre and update state
-    //if filter "All" display all books in local storage
-    if (genre === 'All'){
-      console.log("IT IS ALL BOOKS");
-      const allBooks = JSON.parse(localStorage.getItem('books'));
-      setBooks(allBooks);
-    }
-    //else display normal filtered books based on genre
-    else {
-      const filteredBooks = JSON.parse(localStorage.getItem('books')).filter(book => book.genre === genre);
-      setBooks(filteredBooks);
-    }
-  };
+    //fetch books
+    axios.get('http://localhost:7000/books')
+      .then(response => {
+        //update state with the user data
+        let books = response.data; 
+
+        if (genre === 'All'){
+          setBooks(books);
+        }
+        else {
+          const filteredBooks = books.filter(book => book.genre === genre);
+          setBooks(filteredBooks);
+        }
+      })
+      .catch(error => {
+        //console.error('There was an error fetching the users!', error);
+      });
+  }
   
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
